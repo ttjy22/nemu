c978f3fac9a5af921a5531e13a5001fac8095745
tracer-ics2016 <tracer@njuics.org>
2020-10-29 09:26:18 +0800

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:26:18 up  1:07,  1 user,  load average: 0.17, 0.08, 0.19 17930af875b400221e6ab8efea0d6e2eb65f236b

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -125,7 +125,7 @@ helper_fun opcode_table [256] = {
 /* 0x70 */	inv, inv, inv, inv,
 /* 0x74 */	je_i_b, inv, jbe_i_b, ja_i_b,
 /* 0x78 */	inv, inv, inv, inv,
-/* 0x7c */	inv, inv, jle_i_b, inv,// TODO 不懂为啥_v错了
+/* 0x7c */	inv, lea, jle_i_b, inv,// TODO 不懂为啥_v错了
 /* 0x80 */	group1_b, group1_v, inv, group1_sx_v,
 /* 0x84 */	inv, test_r2rm_v, inv, inv,
 /* 0x88 */	mov_r2rm_b, mov_r2rm_v, mov_rm2r_b, mov_rm2r_v,
