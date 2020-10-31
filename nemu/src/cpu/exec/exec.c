98a5ce8f2daaad078c9dd4e170f8828382d19632
tracer-ics2016 <tracer@njuics.org>
2020-10-31 20:01:15 +0800

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:01:15 up  4:42,  1 user,  load average: 0.11, 0.07, 0.02 ffb05729a044e800184f33c27dc896068e03aa19

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -110,7 +110,7 @@ helper_fun opcode_table [256] = {
 /* 0x34 */	inv, inv, inv, inv,
 /* 0x38 */	cmp_r2rm_b, cmp_r2rm_v, cmp_r2rm_b, cmp_rm2r_v,// TODO cmp_r2rm_v:fib打点 && switch、to-lower-case hit bad trap
 /* 0x3c */	inv, inv, inv, inv,
-/* 0x40 */	inv, inc_r_v, inv, inv,
+/* 0x40 */	inv, inc_r_v, inv, inc_r_v,
 /* 0x44 */	inv, inv, inv, inv,
 /* 0x48 */	dec_r_v, inv, inv, dec_r_v,
 /* 0x4c */	inv, inv, inv, inv,
