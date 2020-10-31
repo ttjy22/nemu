4f289460475f626076ce490f78cac9f4ee47c113
tracer-ics2016 <tracer@njuics.org>
2020-10-31 16:09:49 +0800

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:09:49 up  1:13,  1 user,  load average: 0.01, 0.02, 0.00 cc1dd1985cc78fcebfea032696b53a12cffe933a

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -11,7 +11,7 @@ make_helper (concat(call_i_, SUFFIX))
 {
     int len = concat(decode_i_, SUFFIX) (eip + 1);
     reg_l (R_ESP) -= DATA_BYTE;
-    swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len);
+    swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len+1);
     DATA_TYPE_S displacement = op_src->val;
     print_asm("call %x",cpu.eip + 1 + len + displacement);
     cpu.eip +=displacement;
@@ -21,7 +21,7 @@ make_helper (concat(call_rm_, SUFFIX))
 {
     int len = concat(decode_rm_, SUFFIX) (eip + 1);
     reg_l (R_ESP) -= DATA_BYTE;
-    swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len);
+    swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len+1);
     DATA_TYPE_S displacement = op_src->val;
     print_asm("call %x",displacement);
     cpu.eip = displacement - len - 1;
