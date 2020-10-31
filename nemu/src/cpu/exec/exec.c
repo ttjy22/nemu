25c0e8d3fa6ec81a6ea0e50ff5d0ac793fa407c1
tracer-ics2016 <tracer@njuics.org>
2020-10-31 15:26:03 +0800

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:26:03 up 29 min,  1 user,  load average: 0.03, 0.09, 0.09 37b3f64537827a04a18b2e50c7ba5ddbf70e988

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -116,7 +116,7 @@ helper_fun opcode_table [256] = {
 /* 0x4c */	inv, inv, inv, inv,
 /* 0x50 */	push_r_v, push_r_v, push_r_v, push_r_v,
 /* 0x54 */	push_r_v, push_r_v, push_r_v, push_r_v,
-/* 0x58 */	inv, inv, inv, inv,
+/* 0x58 */	inv, inv, inv, pop_r_v,
 /* 0x5c */	inv, pop_rm_v, inv, inv,
 /* 0x60 */	inv, inv, inv, inv,
 /* 0x64 */	inv, inv, operand_size, inv,
