ef047bb3557e9ca792a2bf14e0c64364a598c0e2
tracer-ics2016 <tracer@njuics.org>
2020-10-31 19:54:17 +0800

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:54:17 up  4:35,  1 user,  load average: 0.08, 0.03, 0.02 7f783a7ab0506ee28282b192592638d21c58e16

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -102,7 +102,7 @@ helper_fun opcode_table [256] = {
 /* 0x14 */	inv, inv, inv, inv,
 /* 0x18 */	inv, inv, inv, inv,
 /* 0x1c */	inv, inv, inv, inv,
-/* 0x20 */	inv, and_r2rm_v, and_r2rm_b, inv,
+/* 0x20 */	inv, and_r2rm_v, and_rm2r_b, inv,
 /* 0x24 */	inv, inv, inv, inv,
 /* 0x28 */	inv, sub_r2rm_v, inv, sub_rm2r_v,
 /* 0x2c */	inv, inv, inv, inv,
@@ -110,7 +110,7 @@ helper_fun opcode_table [256] = {
 /* 0x34 */	inv, inv, inv, inv,
 /* 0x38 */	cmp_r2rm_b, cmp_r2rm_v, inv, cmp_rm2r_v,// TODO cmp_r2rm_v:fib打点 && switch、to-lower-case hit bad trap
 /* 0x3c */	inv, inv, inv, inv,
-/* 0x40 */	inv, inv, inv, inv,
+/* 0x40 */	inv, inc_r_v, inv, inv,
 /* 0x44 */	inv, inv, inv, inv,
 /* 0x48 */	dec_r_v, inv, inv, inv,
 /* 0x4c */	inv, inv, inv, inv,
