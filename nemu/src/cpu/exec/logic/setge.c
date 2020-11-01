ac20256ee9b59653a12e30adf468d0d8a4eec238
tracer-ics2016 <tracer@njuics.org>
2020-11-01 20:47:31 +0800

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:47:31 up  2:14,  1 user,  load average: 0.00, 0.00, 0.00 d71517e2ca8821014d58385cdcad439415c52e2

---
+++ nemu/src/cpu/exec/logic/setge.c
@@ -0,0 +1,9 @@
+//
+// Created by 浮生未休 on 2020/11/1.
+//
+
+#include "cpu/exec/helper.h"
+
+#define DATA_BYTE 1
+#include "setge-template.h"
+#undef DATA_BYTE
---
+++ nemu/src/cpu/exec/logic/setge.h
@@ -0,0 +1,10 @@
+//
+// Created by 浮生未休 on 2020/11/1.
+//
+
+#ifndef __SETGE_H__
+#define __SETGE_H__
+
+make_helper(setge_rm_b);
+
+#endif
