dff8d57c0c7359c1534f41ee085e19b06999b6e2
tracer-ics2016 <tracer@njuics.org>
2020-11-01 20:15:58 +0800

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:15:58 up  1:42,  1 user,  load average: 0.15, 0.09, 0.02 1a8be9e9bae286284edca151ac63605b74e4b56a

--- nemu/src/cpu/exec/logic/shr-template.h
+++ nemu/src/cpu/exec/logic/shr-template.h
@@ -11,8 +11,20 @@ static void do_execute () {
 	OPERAND_W(op_dest, dest);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+//	panic("please implement me");
+    int len = (DATA_BYTE << 3) - 1;
+    cpu.CF = (dest < op_dest->val);
+    cpu.SF=dest >> len;
+    int s1,s2;
+    s1=op_dest->val>>len;
+    s2=op_src->val>>len;
+    cpu.OF=(s1 != s2 && s2 == cpu.SF) ;
+    cpu.ZF=!dest;
+    OPERAND_W(op_dest, dest);
+    dest ^= dest >>4;
+    dest ^= dest >>2;
+    dest ^= dest >>1;
+    cpu.PF=!(dest & 1);
 	print_asm_template2();
 }
 
