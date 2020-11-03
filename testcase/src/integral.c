0e3a0b49a7d020037344b82149e40fc8e73a974d
tracer-ics2016 <tracer@njuics.org>
2020-11-03 20:09:37 +0800

    >  run 3018001704 ttjy Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:09:37 up  2:07,  1 user,  load average: 0.01, 0.03, 0.00 1ee05440d2c8a801259888c14d72c58b5412bfc7

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -9,9 +9,10 @@ FLOAT f(FLOAT x) {
 FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 	int k;
 	FLOAT s,h;
-    set_bp();
+//    set_bp();
 	h = F_div_int((b - a), n);
-	s = F_div_int(fun(a) + fun(b), 2 );
+    set_bp();
+    s = F_div_int(fun(a) + fun(b), 2 );
 	for(k = 1; k < n; k ++) {
 		s += fun(a + F_mul_int(h, k));
 	}
